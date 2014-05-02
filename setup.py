e90a9129cf62b0a5536bcf6f7cdf779416483fed
sernatin <paul.sernatinger@nokia.com>
2014-05-02 17:51:47 -0500

    Got started

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='vdscrypter',
     version='0.00',
-    packages=find_packages('src'),
+    packages=find_packages('.'),
     url='',
     license='',
     author='Oscillot',
--- src/vdscrypter/core.py
+++ src/vdscrypter/core.py
@@ -1,3 +1,6 @@
+import os
+import time
+import tempfile
 from PIL import Image
 
 from vdscrypter.exceptions import InitializationError
@@ -7,9 +10,40 @@ class Video(object):
     """An uninitialized video object"""
     def __init__(self):
         self.initialized = False
+        self.workspace = tempfile.mkdtemp()
+        self.original_file = None
+        self.temp_file = os.path.join(self.workspace, str(int(time.time())))
+
+    def prevent_multiple_initializations(self):
+        if self.initialized:
+            raise InitializationError('Already initialized.')
+
+    def check_exists(self, asset):
+        if os.path.exists(asset):
+            self.original_file = asset
+            self.initialized = True
+        else:
+            raise ValueError('Not a valid file path!')
+
+    def is_image(self):
+        for fmat in ['.bmp', '.jpg', '.jpeg', 'gif', '.tga']:
+            if self.original_file.endswith(fmat):
+                return True
+        return False
+
+    def fix_image(self):
+        raise NotImplemented
 
     def new(self, width, height, color="black"):
         """Initializes the video object as a new video"""
-        if self.initialized:
-            raise InitializationError('Already initialized.')
+        self.prevent_multiple_initializations()
         image = Image.new("RGB", (width, height), color)
+        image.save('%s.bmp' % self.temp_file)
+        self.initialized = True
+
+    def open(self, asset):
+        """Initializes the video object using an existing asset"""
+        self.prevent_multiple_initializations()
+        self.check_exists(asset)
+        if self.is_image():
+            self.fix_image()
